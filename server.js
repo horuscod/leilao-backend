c75a21c3837ebb62c1c1ac8fbcb8488a02911ba2
horuscod <jessicabueno.contato2022@gmail.com>
2023-10-16 17:03:25 -0300

    remove process of the port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const process  = require('dotenv');
 process.config();
 
-const port = process.env.PORT || 1998; 
+const port = 1998; 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
