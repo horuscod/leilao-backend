0ef6d9b4cf60e1adefd19ce8336c290752c0e48a
horuscod <jessicabueno.contato2022@gmail.com>
2023-10-16 16:47:03 -0300

    add server item

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const helmet = require('helmet');
+const cors = require('cors');
+
+const port = process.env.PORT || 1998; 
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(helmet());
+app.use(cors());
+
+const whitelist = ['http://localhost:9000', 'http://192.168.18.7:9000'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+    res.send('Olá, Hórus!');
+});
+
+app.get('/horus', (req, res)=>{
+    res.send('Hey Hórus, Go to Jokes hehehe');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
