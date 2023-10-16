27f617bec2c7791c72505c2a4f22cb5b3853155e
horuscod <jessicabueno.contato2022@gmail.com>
2023-10-16 17:14:36 -0300

    prettier

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const helmet = require('helmet');
-const cors = require('cors');
-const process  = require('dotenv');
+const helmet = require("helmet");
+const cors = require("cors");
+const process = require("dotenv");
 process.config();
 
-const port = 1998; 
+const port = 1998;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
 
-const whitelist = ['http://localhost:9000', 'http://192.168.18.7:9000'];
+const whitelist = ["http://localhost:9000", "http://192.168.18.7:9000"];
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
     }
+  },
 };
 
 app.use(cors(corsOptions));
 
-app.get('/', (req, res) => {
-    res.send('Olá, Hórus!');
+app.get("/", (req, res) => {
+  res.send("Olá, Hórus!");
 });
 
-app.get('/horus', (req, res)=>{
-    res.send('Hey Hórus, Go to Jokes hehehe');
+app.get("/horus", (req, res) => {
+  res.send("Hey Hórus, Go to Jokes hehehe");
 });
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
