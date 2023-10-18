ddb2c02aba6908a33b4355295cac6a28b6f2f9c6
horuscod <jessicabueno.contato2022@gmail.com>
2023-10-18 19:26:25 -0300

    teste product

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,21 +1,28 @@
 const admin = require("firebase-admin");
 require("dotenv").config(); // Carrega as variáveis de ambiente do arquivo .env
 
-admin.initializeApp({
+console.log(JSON.stringify(process.env.FIREBASE_PROJECT_ID))
+console.log('VARIAVEEEEEELLLLL ___________________________')
+console.log(process.env.FIREBASE_PROJECT_ID)
+
+/* admin.initializeApp({
   credential: admin.credential.cert({
     type: "service_account",
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_API_KEY_ID,
-    private_key: process.env.FIREBASE_API_KEY,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url:
-      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+    project_id: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+    private_key_id: JSON.stringify(process.env.FIREBASE_API_KEY_ID),
+    private_key: JSON.stringify(process.env.FIREBASE_API_KEY),
+    client_email: JSON.stringify(process.env.FIREBASE_CLIENT_EMAIL),
+    client_id: JSON.stringify(process.env.FIREBASE_CLIENT_ID),
+    auth_uri: JSON.stringify(process.env.FIREBASE_AUTH_URI),
+    token_uri: JSON.stringify(process.env.FIREBASE_TOKEN_URI),
+    auth_provider_x509_cert_url: JSON.stringify(
+      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL
+    ),
+    client_x509_cert_url: JSON.stringify(
+      process.env.FIREBASE_CLIENT_X509_CERT_URL
+    ),
+    universe_domain: JSON.stringify(process.env.FIREBASE_UNIVERSE_DOMAIN),
   }),
-});
+}); */
 
 module.exports = admin;
