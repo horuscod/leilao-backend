5fad4c35b78f482b65351a5f33178aa7bb571c87
horuscod <jessicabueno.contato2022@gmail.com>
2023-10-18 16:59:14 -0300

    add routes backoffice

--- api/routes/usersBackoffice.js
+++ api/routes/usersBackoffice.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const usersBackOfficeController = require("../controllers/usersBackofficeController");
 
 router.post("/newUserAdmin", usersBackOfficeController.createNewAdminUser);
+router.get("/allUsersAdmin", (req, res) => {
+  res.send(
+    "Você é curioso em para achar essa rota aqui hahaha, para com isso porque é feio viu"
+  );
+});
 router.post("/editUserAdmin", usersBackOfficeController.editAdminUser);
 router.post("/deleteUserAdmin", usersBackOfficeController.deleteAdminUser);
 
