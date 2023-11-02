1cb41dfb6671805482cf64aaa9323b04e631941b
horuscod <jessicabueno.contato2022@gmail.com>
2023-11-02 13:29:08 -0300

    adjust cors

--- server.js
+++ server.js
@@ -26,6 +26,13 @@ const corsOptions = {
   },
 };
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(cors(corsOptions));
 
 /* BackOffice Routes */
